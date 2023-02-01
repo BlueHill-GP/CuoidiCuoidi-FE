7f8ffc480c5f31ba389af493e762813abf19134f
HieuDEVELOPER <hieunguyentx0010@gmail.com>
2023-02-01 16:02:43 +0700

    Update

---
+++ src/components/Posts.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Posts() {
+  return (
+    <div>
+      <h1>Posts</h1>
+      <div>
+        <div className='header_Post'>
+          <img src="" alt="" />
+          <p>name</p>
+          <p>date</p>
+        </div>
+        <p>add friend icon</p>
+      </div>
+    </div>
+  )
+}
+
+export default Posts
