78ce31abcdfefbf22935bcf9e8f8896cf2eb387d
linhanna <linh.nguyenthingoc23@student.passerellesnumeriques.org>
2023-02-15 16:50:09 +0700

    update Regiter and Login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-
-import './App.css';
-import PostsPage from './Pages/PostsPage';
-
+import RegisterPage from "../src/Pages/RegisterPage";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from "../src/Pages/LoginPage";
 function App() {
-  return (
-    <div className="App">
-      <PostsPage/>
-    </div>
-  );
+    return (
+        <main className="App">
+            <Router>
+                <Routes>
+                    <Route path="/" exact element={<RegisterPage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                </Routes>
+            </Router>
+        </main>
+    );
 }
 
 export default App;
---
+++ src/Pages/LoginPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Login from '../components/Login'
+function LoginPage() {
+    return (
+        <div>
+            <div>
+                {/*<h1>RegisterPage</h1>*/}
+                <Login/>
+            </div>
+        </div>
+
+    )
+}
+
+export default LoginPage
