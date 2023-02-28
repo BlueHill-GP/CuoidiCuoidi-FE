a76cd7fcfac5bfdddaa1d9c10fb38d2722763a73
linhanna <linh.nguyenthingoc23@student.passerellesnumeriques.org>
2023-02-28 08:32:26 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from "../src/Pages/LoginPage";
 import PaymentPage from "../src/Pages/PaymentPage";
 import MomoPaymentPage from "../src/Pages/MomoPaymentPage";
+import BookingPage from "../src/Pages/BookingPage";
+import BanUser from "../src/components/BanUser";
 function App() {
     return (
         <main className="App">
@@ -12,6 +14,8 @@ function App() {
                     <Route path="/login" element={<LoginPage />} />
                     <Route path="/Payment" element={<PaymentPage />}  />
                     <Route path="/momo_Payment" element={<MomoPaymentPage />}  />
+                    <Route path="/booking" element={<BookingPage />}  />
+                    <Route path="/banUser" element={<BanUser />}  />
                 </Routes>
             </Router>
         </main>
